2944db83308abb75140cee8b8e668fedd6f11ea8
Bella Amandine <bellamandy45@gmail.com>
2020-10-04 18:56:04 +0200

    make what we do section dynamic using jquery and css

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+$("document").ready(function() {
+
+    $("#design-description-img").click(function() {
+        $("#design-description-img").toggle();
+        $("#design-description").toggle();
+    });
+
+    $("#design-description").click(function() {
+        $("#design-description-img").toggle();
+        $("#design-description").toggle();
+    });
+
+    $("#development-description-img").click(function() {
+        $("#development-description-img").toggle();
+        $("#development-description").toggle();
+    });
+
+    $("#development-description").click(function() {
+        $("#development-description-img").toggle();
+        $("#development-description").toggle();
+    });
+
+    $("#product-description-img").click(function() {
+        $("#product-description-img").toggle();
+        $("#product-description").toggle();
+    });
+
+    $("#product-description").click(function() {
+        $("#product-description-img").toggle();
+        $("#product-description").toggle();
+    });
+    
+})
